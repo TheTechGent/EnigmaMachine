0cca51c85136b76fdcca8190971c0a4907eaf048
The Tech Gent <dalegent@hotmail.com>
2025-03-03 15:12:31 +0000

    Initial Commit

---
+++ PyNigma/rotor.py
@@ -0,0 +1,40 @@
+"""
+I = EKMFLGDQVZNTOWYHXUSPAIBRCJ, Q
+
+II = AJDKSIRUXBLHWTMCQGZNPYFVOE, E
+
+III = BDFHJLCPRTXVZNYEIWGAKMUSQO, V
+
+"""
+
+class Rotor:
+
+    def __init__(self, wiring, notch) -> None:
+        self.left = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        self.right = wiring
+        self.notch = notch
+
+    def forward(self, signal):
+        letter = self.right[signal]
+        signal = self.left.find(letter)
+        return signal
+
+    def backward(self, signal):
+        letter = self.left[signal]
+        signal = self.right.find(letter)
+        return signal
+    
+    def show(self):
+        print(self.left)
+        print(self.right)
+        print("")
+
+    def rotate(self):
+        self.left = self.left[1:] + self.left[0]
+        self.right = self.right[1:] + self.right[0]
+
+I = Rotor("EKMFLGDQVZNTOWYHXUSPAIBRCJ", "Q")
+
+II = Rotor("AJDKSIRUXBLHWTMCQGZNPYFVOE", "E")
+
+III = Rotor("BDFHJLCPRTXVZNYEIWGAKMUSQO", "V")
---
+++ PyNigma/testing.py
@@ -0,0 +1,11 @@
+"""
+Testing string operations
+"""
+
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+for letter in alphabet:
+
+    rotorspin = alphabet[1:] + alphabet[0]
+    alphabet = rotorspin
+    print(alphabet)
